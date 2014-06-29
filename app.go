95ed8d8fd8b7cf096f84747b0fada7e338e8f53f
bobbyduhbrain <Mike@d4-50-160-48.try.wideopenwest.com>
2014-06-28 20:06:09 -0400

    Set security key.

--- app.go
+++ app.go
@@ -3,12 +3,14 @@ package main
 import(
   "github.com/go-martini/martini"
   "github.com/stretchr/gomniauth"
+  "github.com/stretchr/signature"
 )
 
 func main(){
+  gomniauth.SetSecurityKey(signature.RandomKey(64))
   m := martini.Classic()
   m.Get("/", func() string {
-    return "Hello world!"
+    return "Hello poop!"
   })
   m.Run()
 }
