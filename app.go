b26b1bd7449c1d58bd7ed94d08e933292fccffa0
bobbyduhbrain <Mike@d4-50-160-48.try.wideopenwest.com>
2014-06-28 23:31:57 -0400

    Martini uses templates/layout.tmpl as the app layout.

--- app.go
+++ app.go
@@ -4,11 +4,20 @@ import(
   "github.com/go-martini/martini"
   "github.com/stretchr/gomniauth"
   "github.com/stretchr/signature"
+  "github.com/codegangsta/martini-contrib/render"
 )
 
 func main(){
   gomniauth.SetSecurityKey(signature.RandomKey(64))
+  
+  //* 
+  //  Martini
+  //*
   m := martini.Classic()
+  m.Use(render.Renderer(render.Options{
+    Directory: "templates",
+    Layout:    "layout",
+  }))
   m.Get("/", func() string {
     return "Hello poop!"
   })
